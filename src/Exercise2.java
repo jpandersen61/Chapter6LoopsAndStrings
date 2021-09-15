19ec80756c7ea1336ee9af4426ac7b5ea798515f
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2021-09-15 10:11:48 +0200

    initial

---
+++ src/Exercise2.java
@@ -0,0 +1,19 @@
+public class Exercise2 {
+    public static void main(String[] args) {
+        double result = squareRoot(16);
+        System.out.println(result);
+
+    }
+
+    public static double squareRoot(double a)
+    {
+        double x = a/2;
+
+        for(int i = 0; i < 10; i++)
+        {
+            //something with x
+        }
+
+        return x;
+    }
+}
